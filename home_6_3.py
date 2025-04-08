1f7523709b413d58e9bfc28e18ca5108150b1081
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-04-08 22:49:37 +0300

    estgweyt

--- home_6_3.py
+++ home_6_3.py
@@ -1,1 +1,13 @@
-print(1%10)
+number = int(input("Введіть ціле число: "))
+
+while number > 9:
+    digits = [int(digit) for digit in str(number)]
+    product = 1
+    for digit in digits:
+        product *= digit
+    number = product
+
+print(number)
+
+
+
